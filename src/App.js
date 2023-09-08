e5d5129880b5f7cf6067683db4225c87b9526275
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-09-08 18:42:01 +1000

    added testing fetch component, not reading api key in env properly - need fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <p>sdds</p>
+      <BookReviewCard />
+      <BookReviewCard />
+      <FetchTest />
     </div>
   );
 }
 
-export default App;
+function FetchTest() {
+  const apiKey = process.env.GoogleBooksAPIKey;
+  useEffect(() => {
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${apiKey}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(`x`);
+        console.log(data.json());
+      })
+      .catch((error) => console.log(error));
+  }, [apiKey]);
+}
+
+// function Test() {
+//   return (
+//     <React.Fragment>
+//       <p>Testing</p>
+//     </React.Fragment>
+//   );
+// }
+
+function BookReviewCard() {
+  return (
+    <React.Fragment>
+      <div className="BookReviewCard">
+        {/* Need image */}
+        {/* Need header */}
+        {/* Need description from API? */}
+        <p>BookReviewCard</p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+//  App;
