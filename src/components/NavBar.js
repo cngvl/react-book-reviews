c204baa6f5f8ffe65a668835b2b973a47508e22b
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-09-19 15:44:50 +1000

    moved navbar to components folder, currently hardcoded list items - should use state?

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="NavBar">
+        <ul>
+          <li>X</li>
+          <li>Y</li>
+          <li>Z</li>
+          <li>A</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
