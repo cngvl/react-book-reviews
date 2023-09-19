a2af4005126bdc9b0298a6c22416d687fd52aa30
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-09-19 20:10:30 +1000

    playing around with onchange parameter, calling function not working - maybe due to scope issue?

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = (search) => {
   return (
     <>
-      <div className="SearchBar">x</div>
+      <div className="SearchBar">
+        <input
+          type="text"
+          placeholder="Enter title of book"
+          // onChange={(event) => onChangeTest()}
+          onChange={(event) => console.log(event.nativeEvent.data)}
+        />
+        <button>Submit</button>
+      </div>
     </>
   );
 };
