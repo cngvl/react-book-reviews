d62e97074b7897ff939fe3764335e99ea22d8ec1
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-09-20 15:55:39 +1000

    testing by moving the fetch method into this component file but getting hook error

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const SearchBar = (search) => {
+function SearchBar(search) {
+  // Not sure if it makes much sense for the function to be here?
+  function onChangeTest() {
+    console.log("calling onChangeTest");
+  }
+
+  function FetchTest() {
+    const apiKey = process.env.REACT_APP_GOOGLEBOOKS_APIKEY;
+    // console.log(apiKey);
+    useEffect(() => {
+      fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${apiKey}`
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => console.log(error));
+    }, [apiKey]);
+  }
+
   return (
     <>
       <div className="SearchBar">
         <input
           type="text"
           placeholder="Enter title of book"
-          // onChange={(event) => onChangeTest()}
-          onChange={(event) => console.log(event.nativeEvent.data)}
+          onChange={(event) => {
+            onChangeTest();
+            // FetchTest(); - THIS BREAKS
+          }}
+          // onChange={(event) => console.log(event.nativeEvent.data)}
         />
         <button>Submit</button>
       </div>
     </>
   );
-};
+}
 
 export default SearchBar;
