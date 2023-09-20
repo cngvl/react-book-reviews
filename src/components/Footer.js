761781802aca5eb2901d2ebdee59465e1d019e73
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-09-20 15:54:59 +1000

    changed from funct to const

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-function Footer() {
+const Footer = () => {
   return (
     <>
       <div className="Footer">
-        <p>Suuport me by sending me your life savings pls </p>
+        <p>Support me by sending me your life savings pls </p>
         <a href="https://github.com/cngvl">GitHub</a>
       </div>
     </>
   );
-}
+};
 
 export default Footer;
