6c336619d0c492a42844c6a3e5cd2eb2c653d823
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-09-20 15:19:55 +1000

    creating components file for footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="Footer">
+        <p>Suuport me by sending me your life savings pls </p>
+        <a href="https://github.com/cngvl">GitHub</a>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
